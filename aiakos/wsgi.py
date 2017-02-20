07775dee78830c5aa09ec98f57ae0dc73f5eabe1
Linus Lewandowski <linus@lew21.net>
2017-02-20 19:40:44 +0100

    Add Werkzeug debugger.

--- aiakos/wsgi.py
+++ aiakos/wsgi.py
@@ -14,3 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
+
+DEBUG = bool(int(os.environ.get("DEBUG", "0")))
+if DEBUG:
+	from django.views import debug
+	def null_technical_500_response(request, exc_type, exc_value, tb, status_code=500):
+		raise exc_value.with_traceback(tb)
+	debug.technical_500_response = null_technical_500_response
+
+	from werkzeug.debug import DebuggedApplication
+	application = DebuggedApplication(application, evalex=True, pin_security=False)
