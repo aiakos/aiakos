cb706d14d5ec4b4f7531224591e7b5d99add1510
Tomasz Święcicki <tswiecicki@opera.com>
2017-02-28 12:53:18 +0100

    Add view for password change
    * we use PasswordChangeForm from django

---
+++ aiakos/views/password_management.py
@@ -0,0 +1,24 @@
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+
+
+def password_change(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(
+                request, 'Your password has been successfully updated!'
+            )
+            return redirect('custom-password-change')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/password_change.html', {
+        'form': form
+    })
