44b6bf11cc610c1f40a86f1e111a56aa6074d462
Linus Lewandowski <llewandowski@opera.com>
2017-03-01 12:11:51 +0100

    Fix can_reset_password.

--- aiakos/models.py
+++ aiakos/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import User
 
 
 def can_reset_password(self):
-	return self.has_usable_password() or len(self.external_identities) == 0
+	return self.has_usable_password() or len(self.externalidentity_set) == 0
 
 User.can_reset_password = property(can_reset_password)
