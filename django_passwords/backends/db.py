df3929f133035fb30e154a353dc976edbbd2017e
Linus Lewandowski <llewandowski@opera.com>
2017-09-29 14:11:33 +0200

    Restore fallback for users without usable passwords.

--- django_passwords/backends/db.py
+++ django_passwords/backends/db.py
@@ -1,13 +1,13 @@
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
 
 class Backend:
 	def __init__(self, url):
 		pass
 
 	def check_password(self, user, password):
-		return user.check_password(password)
+		ret = user.check_password(password)
+		return ret if user.has_usable_password() else None
 
 	def set_password(self, user, password):
 		user.set_password(password)
